aac3f00c0ffb375e2175db12c0788892670338a2
leirbag1305 <ghfs.15@gmail.com>
2021-03-25 18:34:05 -0300

    Conceito Django

--- core/views.py
+++ core/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def hello(request, nome, idade):
-    return HttpResponse('<h1>Hello {} de {} anos</h1>' .format(nome, idade))
+def soma(request, num1, num2):
+    saida = num1 + num2
+    return HttpResponse('<h1>Soma de {} + {} = {}</h1>' .format(num1, num2, saida))
+def subtracao(request, num1, num2):
+    saida = num1 - num2
+    return HttpResponse('<h1>Subtração de {} - {} = {}</h1>' .format(num1, num2, saida))
+def multiplicacao(request, num1, num2):
+    saida = num1 * num2
+    return HttpResponse('<h1>Multiplicação de {} * {} = {}</h1>' .format(num1, num2, saida))
+def divisao(request, num1, num2):
+    saida = num1 / num2
+    return HttpResponse('<h1>Divisão de {} / {} = {}</h1>' .format(num1, num2, saida))
